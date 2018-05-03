1fe9fca2e50857dda3c8a3ae01614e96f0938244
ChristianLlanas <36010216+ChristianLlanas@users.noreply.github.com>
2018-05-03 16:05:10 -0500

    Updated file

--- src/Clock.java
+++ src/Clock.java
@@ -309,7 +309,7 @@ public class Clock extends javax.swing.JFrame {
             if (ocTime >= 0) {
 
                 if (ocTime == 0) {
-                    finalTimeString = "00:00"
+                    finalTimeString = "00:00";
                 } else if (ocTime >= 1000) {
                     finalTimeString = "+" + " " + protoTimeString.substring(0, 2);
                     finalTimeString += ":" + protoTimeString.substring(2, 4);
@@ -351,41 +351,10 @@ public class Clock extends javax.swing.JFrame {
 
         timeClock2.start();
     }//GEN-LAST:event_oTimeZoneScrollActionPerformed
-
+   
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Clock.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Clock.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Clock.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Clock.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Clock().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel cTimeLabel;
